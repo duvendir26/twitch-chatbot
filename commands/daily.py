8f72f8b9fd643fea375c70a972a84719bf1a20cf
stefanrogic <stefanrogic@protonmail.com>
2026-08-24 11:26:55 +0200

    bugfixes and refactoring

--- commands/daily.py
+++ commands/daily.py
@@ -1,11 +1,15 @@
 import random
 
-DAILY_MIN = 0
-DAILY_MAX = 25
+from time import time
+from utils.users import get_user, set_user
+
+DAILY_MIN = 1
+DAILY_MAX = 30
 
 GAIN_CHANCE = 0.8
 LOSE_CHANCE = 0.2
 
+COOLDOWN_TIME = 15 * 60
 COOLDOWN_IMMUNITY = ["the_kekbot"]
 
 messagesZero = [
@@ -16,18 +20,33 @@ messagesZero = [
 
 messagesGain = [
     "You found a hidden stash of keks KEKShook",
-    "You won a kek lottery KEKP Clap",
+    "You inherited some keks from a distant relative KEKP Clap",
     "You were gifted keks by a generous stranger KEKScreen",
 ]
 
 messagesLose = [
     "You gave your keks to a stranger KEKP Love",
-    "You were abducted by aliens and they took your keks AlienPls KEKP",
+    "You were abducted by aliens and they took your keks AlienPls KEK7",
     "You donated your keks to charity KEKP Love",
+]
+
+messagesTime = [
+    "You must wait [ {minutes}m {seconds}s ] minutes before claiming your next daily reward KEKScreen",
+    "You are too early to collect your keks. Please wait [ {minutes}m {seconds}s ] minutes KEKScreen",
+    "You have already collected your keks today. Come back in [ {minutes}m {seconds}s ] minutes KEKScreen",
 ]
 
 
 async def cmd_daily(username, reply, args=None):
+    user = get_user(username)
+    last_daily = int(user["last_daily"])
+    
+    minutes = int((last_daily + COOLDOWN_TIME - time()) / 60)
+    seconds = int((last_daily + COOLDOWN_TIME - time()) % 60)
+    if username not in COOLDOWN_IMMUNITY and last_daily + COOLDOWN_TIME > time():
+        await reply(f"@{username} {random.choice(messagesTime).format(minutes=minutes, seconds=seconds)}")
+        return
+    
     amount = random.randint(DAILY_MIN, DAILY_MAX)
     roll = random.random()
     
@@ -35,7 +54,7 @@ async def cmd_daily(username, reply, args=None):
         await reply(
             f"@{username} {random.choice(messagesZero)} | +0 🍪"
         )
-    elif roll < GAIN_CHANCE:
+    elif roll < GAIN_CHANCE and not roll == 0.05:
         await reply(
             f"@{username} {random.choice(messagesGain)} | +{amount} 🍪"
         )
@@ -43,3 +62,11 @@ async def cmd_daily(username, reply, args=None):
         await reply(
             f"@{username} {random.choice(messagesLose)} | -{amount} 🍪"
         )
+        
+    if roll < GAIN_CHANCE:
+        user["balance"] += amount
+    else:
+        user["balance"] -= amount
+        
+    user["last_daily"] = int(time())
+    set_user(username, user)
---
+++ utils/users.py
@@ -0,0 +1,49 @@
+import json
+
+USERS_FILE = "data/users.json"
+
+
+def load_users():
+    with open(USERS_FILE, "r") as f:
+        return json.load(f)
+
+
+def save_users(users):
+    with open(USERS_FILE, "w") as f:
+        json.dump(users, f, indent=4)
+
+
+def add_user(username):
+    users = load_users()
+
+    if not any(user["username"] == username for user in users):
+        users.append({
+            "username": username,
+            "balance": 0,
+            "last_daily": 0
+        })
+
+        save_users(users)
+
+
+def get_user(username):
+    users = load_users()
+
+    for user in users:
+        if user["username"] == username:
+            return user
+
+    add_user(username)
+
+    return get_user(username)
+
+
+def set_user(username, user_data):
+    users = load_users()
+
+    for i, user in enumerate(users):
+        if user["username"] == username:
+            users[i] = user_data
+            break
+
+    save_users(users)
